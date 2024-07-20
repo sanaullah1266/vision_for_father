4db3af95702926e25e727d594c8aa6fb6e2bd1f7
unknown <Administrator@PC14-LAB8.labs.org>
2024-07-20 14:14:07 +0500

    vision start

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Http\Request;
+use App\Breadcrumbs\Breadcrumbs;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Schema::defaultStringLength(191);
+        Paginator::useBootstrapFive();
+
+        Request::macro('breadcrumbs', function (){
+            return new Breadcrumbs($this);
+        });
+    }
+}
